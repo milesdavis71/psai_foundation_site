cbe346c3923cdff26e10a43a2a16fbff0a4eee0b
milesdavis71 <hevesiistvan71@hotmail.com>
2020-06-02 07:56:55 +0200

    Régi menü beemelve, átdolgozni link megmaradósra. gulpfile.babel.js-be beírva a php figyelése, mint az orig masterben.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -163,6 +163,10 @@ function reload(done) {
 function watch() {
   gulp.watch(PATHS.assets, copy);
   gulp.watch('src/pages/**/*.html').on('all', gulp.series(pages, browser.reload));
+  //php
+  gulp.watch('src/pages/**/*.php').on('all', gulp.series(pages, browser.reload));
+
+
   gulp.watch('src/{layouts,partials}/**/*.html').on('all', gulp.series(resetPages, pages, browser.reload));
   gulp.watch('src/data/**/*.{js,json,yml}').on('all', gulp.series(resetPages, pages, browser.reload));
   gulp.watch('src/helpers/**/*.js').on('all', gulp.series(resetPages, pages, browser.reload));
