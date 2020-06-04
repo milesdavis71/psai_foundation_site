730c98dd9bea9d27669f401be5b031b7b12e401c
milesdavis71 <hevesiistvan71@hotmail.com>
2020-06-04 10:37:21 +0200

    felvételi lehetőségről szóló tájékoztatónál tartok

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -163,9 +163,8 @@ function reload(done) {
 function watch() {
   gulp.watch(PATHS.assets, copy);
   gulp.watch('src/pages/**/*.html').on('all', gulp.series(pages, browser.reload));
-  //php
+  // php-t figyeli (galéria fájlok)
   gulp.watch('src/pages/**/*.php').on('all', gulp.series(pages, browser.reload));
-
 
   gulp.watch('src/{layouts,partials}/**/*.html').on('all', gulp.series(resetPages, pages, browser.reload));
   gulp.watch('src/data/**/*.{js,json,yml}').on('all', gulp.series(resetPages, pages, browser.reload));
