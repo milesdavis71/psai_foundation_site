2561c74c1c0266519fa8a863353443e1016fd725
milesdavis71 <OttosMops71>
2021-03-31 11:26:44 +0200

    2021-es új PS/BS galéria létrehozva.

--- src/pages/galeriak/2020_21/bs/fooldal_2020_21_bs.php
+++ src/pages/galeriak/2020_21/bs/fooldal_2020_21_bs.php
@@ -1,7 +1,34 @@
 {{> hover}}
 
 <div class="cell medium-9 medium-cell-block-y">
-  {{> vissza}}
+<style>
+  .headers {
+    top: 0;
+    position: -webkit-sticky;
+    position: sticky;
+    z-index: 1;
+  }
+
+  .tracks,
+  .scroller {
+    display: flex;
+    overflow-y: hidden;
+    -webkit-overflow-scrolling: touch;
+  }
+  .scroller {
+    overflow-x: hidden;
+  }
+</style>
+
+<div class="headers">
+  <div class="scroller">
+    <ul class="menu simple">
+      <li style="padding: 0.7rem 1rem;"><a href="{{root}}galeriak/2020_21/ps/fooldal_2020_21_ps.php">Székhely</a></li>
+      <li class="is-active"><a href="{{root}}galeriak/2020_21/bs/fooldal_2020_21_bs.php" style="padding: 0.7rem 1rem;">Tagiskola</a></li>
+    </ul>
+  </div>
+</div>
+
 
   <h3 class="text-center">Szegedi Petőfi Sándor Általános Iskola<br />
     Bálint Sándor Tagiskola<br>
