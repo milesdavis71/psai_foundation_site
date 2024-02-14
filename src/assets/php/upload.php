cf1faf8adf0703042bffb595c929401979a81ddc
Istvan <hevesiistvan71@hotmail.com>
2024-02-14 13:39:01 +0100

    2024 mese/prózaverseny formok fejlesztés kész. Upload is kész

--- src/assets/php/upload.php
+++ src/assets/php/upload.php
@@ -1,9 +1,18 @@
 <?php
+
+
 /* Get the name of the uploaded file */
 $filename = $_FILES['file']['name'];
 
+
+$time = gmdate("H-i", time());
+$date = date("Y-m-d");
+$dateTime = $date . "_" . $time . "_";
+
 /* Choose where to save the uploaded file */
-$location = "uploads/" . $filename;
+$location = "uploads/" . $dateTime . $filename;
+
+
 
 /* Save the uploaded file */
 move_uploaded_file($_FILES['file']['tmp_name'], $location);
