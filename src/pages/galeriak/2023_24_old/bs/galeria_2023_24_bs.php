d368c5f83fa94a6dc7744531a4ccab614c67979f
Istvan <hevesiistvan71@hotmail.com>
2024-09-30 14:58:46 +0200

    új galéria fejlesztése folyamatban. Közzétételi lista adatok frissítése.

---
+++ src/pages/galeriak/2023_24_old/bs/galeria_2023_24_bs.php
@@ -0,0 +1,33 @@
+<div class="cell medium-9 medium-cell-block-y">
+    {{> vissza2}}
+
+
+    <h3 class="text-center">Szegedi Petőfi Sándor Általános Iskola<br />
+        Bálint Sándor Tagiskolája<br>
+        <small>
+            <?php echo $_POST["title"]; ?>
+        </small>
+    </h3>
+    <hr>
+
+<div class="gg-container">
+        <h3>Default</h3>
+        <div class="gg-box">
+          <img src="img/1.jpg">
+          <img src="img/2.jpg">
+          <img src="img/3.jpg">
+          <img src="img/4.jpg">
+          <img src="img/5.jpg">
+          <img src="img/6.jpg">
+          <img src="img/7.jpg">
+          <img src="img/8.jpg">
+          <img src="img/9.jpg">
+          <img src="img/10.jpg">
+          <img src="img/11.jpg">
+          <img src="img/12.jpg">
+          <img src="img/13.jpg">
+          <img src="img/14.jpg">
+        </div>
+    </div>
+    {{> grid_gallery_js}}
+    {{> vissza2}}
